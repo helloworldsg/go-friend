22b1526935389cc7bd580224dbe1d93a021ba19b
hmoniaga <harry@moniaga.com>
2018-07-22 04:18:09 +0800

    add user implementation

---
+++ user.go
@@ -0,0 +1,21 @@
+package go_friend
+
+// User represents a registered user based on unique identifier: email
+type User struct {
+	Email       string
+	Friends     map[string]bool
+	Blocked     map[string]bool
+	Followers   map[string]bool
+	Subscribers map[string]bool
+}
+
+// NewUser creates a new user based on unique email
+func NewUser(email string) User {
+	return User{
+		Email:       email,
+		Friends:     make(map[string]bool),
+		Blocked:     make(map[string]bool),
+		Followers:   make(map[string]bool),
+		Subscribers: make(map[string]bool),
+	}
+}
---
+++ user_test.go
@@ -0,0 +1,9 @@
+package go_friend
+
+import "fmt"
+
+func ExampleNewUser() {
+	fmt.Println(NewUser("email@gmail.com").Email)
+
+	// Output: email@gmail.com
+}
